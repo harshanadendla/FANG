b7c7ebe87496a206c270a679ae08b1772cd1b62a
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2021-09-18 14:13:26 -0500

    Update 3largestInArray.cpp

--- 3largestInArray.cpp
+++ 3largestInArray.cpp
@@ -3,36 +3,26 @@
 #include<climits>
 using namespace std;
 
-void shift(vector<int>& res,int num,int idx)
-{
-    for(int i=0;i<idx;i++)
-    {
+void shift(vector<int>& res,int num,int idx) {
+    for(int i=0;i<idx;i++) {
         res[i]=res[i+1];
     }
     res[idx]=num;
 }
 
-void find3max(vector<int> array,vector<int>& res)
-{
-    for(int num : array)
-    {
-        if(num>=res[2])
-        {
+void find3max(vector<int> array,vector<int>& res) {
+    for(int num : array) {
+        if(num>=res[2]) {
            shift(res,num,2);
-        }
-        else if(num>=res[1])
-        {
+        } else if(num>=res[1]) {
            shift(res,num,1);
-        }
-        else if(num>=res[0])
-        {
+        } else if(num>=res[0]) {
            shift(res,num,0); 
         }
     }
 }
 
-int main()
-{
+int main() {
     vector<int> array {/*.......*/};
     vector<int> res(3,INT_MIN);
     find3max(array,res);
