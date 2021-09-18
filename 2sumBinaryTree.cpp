65f6ba1bc7a8f43755f33d2451ce83a4c9121cd9
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2021-09-18 13:50:14 -0500

    Update 2sumBinaryTree.cpp

--- 2sumBinaryTree.cpp
+++ 2sumBinaryTree.cpp
@@ -1,54 +1,35 @@
-bool 2sumInBinaryTree(TreeNode* a,int target)
-{
+bool 2sumInBinaryTree(TreeNode* a,int target) {
     stack<TreeNode*> s1,s2;
-
     TreeNode* temp1=a;
     TreeNode* temp2=a;
-
-    while(temp1)
-    {
+    while(temp1) {
         s1.push(temp1);
         temp1=temp1->left;
     }
-    while(temp2)
-    {
+    while(temp2) {
         s2.push(temp2);
         temp2=temp2->right;
     }
-
     temp1=s1.top();
     temp2=s2.top();
-
-    while(temp1 && temp2 && temp1->val < temp2->val)
-    {
-        if(temp1->val + temp2->val == target)
-        {
+    while(temp1 && temp2 && temp1->val < temp2->val) {
+        if(temp1->val + temp2->val == target) {
             return true;
-        }
-        else if(temp1->val + temp2->val < target)
-        {
+        } else if(temp1->val + temp2->val < target) {
             s1.pop();
- 
             temp1=temp1->right;
-            while(temp1)
-            {
+            while(temp1) {
                 s1.push(temp1);
                 temp1=temp1->left;
             }
-
             temp1=s1.top();
-        }
-        else
-        {
+        } else {
             s2.pop();
-
             temp2=temp2->left;
-            while(temp2)
-            {
+            while(temp2) {
                 s2.push(temp2);
                 temp2=temp2->right;
             }
-
             temp2=s2.top();
         }
     }
